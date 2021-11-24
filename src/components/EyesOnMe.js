a408311d82f8e576ef0db26c7dd581771e6ea2b2
Nicholas Shephard <shephardn@gmail.com>
2021-11-24 10:43:26 -0600

    commit tree 09ec5b50159aebbb6ab2aa3d60f37a62ed18fbf1

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(props) {
+
+    return (
+        <div className="eyes">
+            <button onFocus={()=>console.log("Good!")} onBlur={()=>console.log("Hey! Eyes on me!")}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(props) {
+
+    return(
+        <div className="keypad">
+            <form onChange={()=>console.log("Entering password...")}>
+                <input type="password" name="password" placeholder="Simpson, eh?" />
+            </form>
+        </div>
+    );
+}
+
+export default Keypad;
